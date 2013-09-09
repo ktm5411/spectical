acd7138b8d11aa9d31e81e8878a46f8e83734304
vadim <just.zimer@gmail.com>
2013-09-10 05:25:47 +0700

    calendars add

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,3 +1,5 @@
 class Calendar < ActiveRecord::Base
   has_and_belongs_to_many :events
+
+  validates_presence_of :name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Spectical::Application.routes.draw do
     get 'schedule', on: :collection
   end
 
+  resources :calendars
   root to:'events#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
